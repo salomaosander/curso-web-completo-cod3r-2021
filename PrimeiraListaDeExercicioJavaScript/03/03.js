5e171b3dda40e6773e7e7dc408f82138fd993a60
Salomão Sander <80934337+salomaosander@users.noreply.github.com>
2021-03-25 17:26:12 -0300

    25-03-21

---
+++ PrimeiraListaDeExercicioJavaScript/03/03.js
@@ -0,0 +1,6 @@
+/* 3. Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
+
+const baseElevada = (base, expoente) => console.log(Math.pow(base, expoente)) 
+// base ** expoente  .... sejuestao de resolução
+
+baseElevada(2,3)
