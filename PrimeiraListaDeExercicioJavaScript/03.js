85fb5511ee5b2d2318a6819858e0beb372f7c36f
Salomão Sander <80934337+salomaosander@users.noreply.github.com>
2024-01-13 22:12:55 -0300

    Delete PrimeiraListaDeExercicioJavaScript directory

--- PrimeiraListaDeExercicioJavaScript/03.js
+++
@@ -1,6 +0,0 @@
-/* 3. Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
-
-const baseElevada = (base, expoente) => console.log(Math.pow(base, expoente)) 
-// base ** expoente  .... sejuestao de resolução
-
-baseElevada(2,3)
--- PrimeiraListaDeExercicioJavaScript/04.js
+++
@@ -1,8 +0,0 @@
-/* 4. Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o 
-resultado e o resto da divisão destes dois valores. */
-
-const divisaoMaisResto = (dividendo, divisor) => {
-    console.log(`Resultado: ${Math.trunc(dividendo / divisor)}, Resto: ${dividendo % divisor}`)
-}
-
-divisaoMaisResto(7,3)
